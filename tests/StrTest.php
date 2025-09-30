9dd527475d03b05907d2151676529f6012a61b00
Candra Sudirman <dev.cndrsdrmn@gmail.com>
2025-10-01 02:10:27 +0700

    feat: introduce `Str::pascal` case conversion

---
+++ tests/StrTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Flockyn\PHPFlock\Str;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class StrTest extends TestCase
+{
+    #[Test]
+    public function it_pascal_conversion(): void
+    {
+        $this->assertSame('FirstName', Str::pascal('first_name'));
+        $this->assertSame('ApiKeyValue', Str::pascal('api-key-value'));
+        $this->assertSame('DatabaseConnection', Str::pascal('DatabaseConnection'));
+        $this->assertSame('UserIdAndUrlPath', Str::pascal('userIDAndURLPath'));
+        $this->assertSame('MixedCaseInputValue', Str::pascal('mixed-case_input Value'));
+        $this->assertSame('FirstName', Str::pascal('firstName'));
+    }
+}
